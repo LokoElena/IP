98a51cf94692d753ee150b5828b6b854b8764629
Doshinsky <akter1997@gmail.com>
2018-09-27 10:54:33 +0700

    Realised a 'baby-step giant-step' algorithm. Some fixes needed

--- Lab-1/cryptolib.c
+++ Lab-1/cryptolib.c
@@ -5,15 +5,27 @@
 #include <string.h>
 #include <math.h>
 #include <time.h>
+#include <sys/time.h>
+
+#include "hashtab.h"
+
+struct listnode *hashtab[HASHTAB_SIZE];
 
 /*	Func section	*/
+
+double wtime()
+{
+    struct timeval t;
+    gettimeofday(&t, NULL);
+    return (double)t.tv_sec + (double)t.tv_usec * 1E-6;
+}
 
 int test_prime_num(unsigned long long int p)
 {
 	unsigned long long int i = 1;
 
 	for (i = 2; i < sqrt(p); ++i) {
-	if (p % i == 0) return 0;
+		if (p % i == 0) return 0;
 	}
 	return 1;
 }
@@ -89,24 +101,57 @@ void euclid(unsigned long long int a, unsigned long long int b, unsigned long lo
 void diffyhellman(unsigned long long int* K1, unsigned long long int* K2)
 {
 	srand(time(NULL));
-	unsigned long long int g = generate_prime_number(1, 1000);//4200;
+	unsigned long long int g = generate_prime_number(1, 1000);
 	unsigned long long int p;
 
-	p = generate_prime_number(1, 1000);//3571;
+	p = generate_prime_number(1, 1000);
 	unsigned long long int KeyA = 0, KeyB = 0;
 	unsigned long long int a, b;
-	unsigned long long int Ka, Kb;
 
-	a = rand() % (1000000 - 10000) + 10000;//187653288;
+	a = rand() % (1000000 - 10000) + 10000;
 	expmod_func(g, a, p, &KeyA);
-	b = rand() % (1000000 - 10000) + 10000;// 987701673;
+	b = rand() % (1000000 - 10000) + 10000;
 	expmod_func(g, b, p, &KeyB);
-        printf ("g %lld, p % lld, a %lld, b %lld, A %lld B %lld\n",g,p,a,b, KeyA, KeyB);
-	expmod_func(KeyB, a, p, &Ka);
-	expmod_func(KeyA, b, p, &Kb);
-	*K1 = Ka;
-	*K2 = Kb;
+
+	expmod_func(KeyB, a, p, &*K1);
+	expmod_func(KeyA, b, p, &*K2);
 }
+
+int child_giant(long long base, long long moduli, long long answer)
+{
+	hashtab_init(hashtab);
+	struct listnode *node;
+	int m = sqrt(moduli) + 2;
+	int k = sqrt(moduli) + 1;
+
+	printf("%d\t%d\n", m, k);
+
+	unsigned long long int *row_y = (unsigned long long int*)malloc(m + 1);
+	unsigned long long int *row_a = (unsigned long long int*)malloc(k + 1);
+
+	double t;
+	t = wtime();
+	
+	for(int i = 0; i < m; i++){
+		expmod_func(base, i, moduli, &row_y[i]);
+		row_y[i] = (row_y[i] * (answer % moduli)) % moduli;
+		hashtab_add(hashtab, row_y[i], i);
+	}
+	int x;
+	for(int j = 1; j < k + 1; j++){
+		expmod_func(base, j * m, moduli, &row_a[j]);
+		node = hashtab_lookup(hashtab, row_a[j]);
+		if(node != NULL){
+				x = j * m - node->value;
+				t = wtime() - t;
+//				printf("Time: %.6f sec.\n", t);
+				return x;
+			}
+//		}
+	}
+	return -1;
+}
+
 
 /*	Main section	*/
 
@@ -135,10 +180,12 @@ int main(int argc, char const *argv[])
 	expmod_func(base, exponent, module, &fem_res);
 	euclid(euclid_a, euclid_b, euclid_res);
 	diffyhellman(&KeyA, &KeyB);
+	int child_giant_res = child_giant(2, 23, 9);
 
 	printf("FEM's result:\t%lld\n", fem_res);
 	printf("GEA's result:\t%lld, %lld and their GCD: %lld\n", euclid_res[0], euclid_res[1], euclid_res[2]);
 	printf("DH's result:\t%lld = %lld\n", KeyA, KeyB);
+	printf("CGS's result:\t%d\n", child_giant_res);
 
 	return EXIT_SUCCESS;
 }
